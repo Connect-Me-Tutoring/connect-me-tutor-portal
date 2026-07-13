bacd1887a8107801a614cec9f8061dff4031890b
Sarthak Tayal <sarthaktayal2@gmail.com>
2026-07-13 00:27:33 -0400

    add layered circles to welcome panel

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,23 @@ export default async function LoginPage() {
   return (
     <main className="min-h-svh bg-white text-[#071329] lg:grid lg:min-h-[760px] lg:grid-cols-[48%_52%]">
       <section className="relative min-h-[250px] overflow-hidden bg-[#dceeff] px-6 py-7 sm:min-h-[300px] sm:px-10 lg:min-h-svh lg:px-[10%] lg:py-12">
+        <div
+          aria-hidden="true"
+          className="absolute -left-8 top-[22%] h-20 w-20 rounded-full bg-[#f4f0c9] sm:h-24 sm:w-24 lg:-left-10 lg:top-[18%]"
+        />
+        <div
+          aria-hidden="true"
+          className="absolute -right-[22%] top-[16%] h-[390px] w-[390px] rounded-full bg-[#bfe8f5]/70 sm:-right-[12%] lg:-right-[28%] lg:h-[620px] lg:w-[620px]"
+        />
+        <div
+          aria-hidden="true"
+          className="absolute -bottom-32 -left-24 h-64 w-64 rounded-full bg-[#f4f0c9] lg:-bottom-40 lg:-left-28 lg:h-[350px] lg:w-[350px]"
+        />
+        <div
+          aria-hidden="true"
+          className="absolute bottom-[7%] right-[15%] hidden h-24 w-24 rounded-full bg-[#b9dcff]/80 lg:block"
+        />
+
         <div className="relative z-10 flex items-center gap-3">
           <Image
             alt="Connect Me globe"
